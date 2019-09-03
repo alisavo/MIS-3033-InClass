7fa0ab86e5115ae6861242073fd153317340bc28
Alisa Vo <alisa.l.vo@ou.edu>
2019-09-03 03:01:51 -0500

    update loop1

--- HW1 3013 Review/Loops1/Program.cs
+++ HW1 3013 Review/Loops1/Program.cs
@@ -10,6 +10,20 @@ namespace Loops1
     {
         static void Main(string[] args)
         {
+            int examGrades, studentAverage=0;
+            int studentScore;
+
+            for (examGrades=1; examGrades<=3; examGrades++)
+            {
+                Console.Write("Please enter your exam score: ");
+                studentScore = Convert.ToInt32(Console.ReadLine());
+                studentAverage = (studentScore+studentScore+studentScore)/3;
+            }
+
+            Console.WriteLine($"Your average exam grade is: {studentAverage}%");
+
+            Console.ReadKey();
+
         }
     }
 }
