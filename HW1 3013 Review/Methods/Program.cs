fc628faec0f977ceea1ebdc9a0ffff48221043fc
Alisa Vo <alisa.l.vo@ou.edu>
2019-09-03 23:16:11 -0500

    update

--- HW1 3013 Review/Methods/Program.cs
+++ HW1 3013 Review/Methods/Program.cs
@@ -8,12 +8,36 @@ namespace Methods
 {
     class Program
     {
+        static double LineValueForY(double m, double x, double b)
+        {
+            double Y;
+            Y = (m * x) + b;
+            return Y;
+        }
+
         static void Main(string[] args)
         {
+            double inputSlope, xValue, yValue;
+            double yIntercept;
+            Console.WriteLine("What is the value of your slope? ");
+            inputSlope = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the value of x?");
+            xValue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the value of your Y-Intercept? ");
+            yIntercept = Convert.ToDouble(Console.ReadLine());
+
+            yValue = LineValueForY(inputSlope, xValue, yIntercept);
+            Console.WriteLine($"Y= {yValue}");
+
+            Console.ReadKey();
+
+            
+
 
 
 
 
         }
+
     }
 }
