0acd305b856afa25dbcadd1b7629ecf5412d066b
Alisa Vo <alisa.l.vo@ou.edu>
2019-09-04 11:09:52 -0500

    dates

--- 942019/942019/Program.cs
+++ 942019/942019/Program.cs
@@ -12,9 +12,17 @@ namespace _942019
         {
             DateTime today = DateTime.Now;
             today.AddDays(26);
-
+            DateTime future = today.AddDays(27);
 
             Console.WriteLine(today);
+            Console.WriteLine(future);
+            Console.WriteLine(today.ToLongDateString());
+
+            var difference = future - today;
+            Console.WriteLine(difference.Days);  //.days makes it the difference in days, without it it would be 27:00:00
+
+
+
             Console.ReadKey();
         }
     }
